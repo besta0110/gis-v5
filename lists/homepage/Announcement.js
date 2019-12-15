267870d1b2eeefbadf8eb8936466087f87f9c338
seday72 <seday72@yandex.com>
2019-12-15 20:17:37 +1000

    add assets

---
+++ lists/homepage/Announcement.js
@@ -0,0 +1,23 @@
+const { Text, File, CloudinaryImage } = require('@keystonejs/fields');
+const { fileAdapter, cloudinaryAdapter } = require('../FileAdapters');
+const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce');
+
+module.exports = {
+  fields: {
+    image: {
+      type: CloudinaryImage,
+      adapter: cloudinaryAdapter
+    },
+    title: {
+      type: Text,
+      isRequired: true
+    },
+    brief: { 
+      type: Text,
+      isRequired: true
+    },
+    extended: {
+      type: Wysiwyg
+    },
+  }
+}
