04c81780042556194250c28e126ddd39f2ac2210
seday72 <seday72@yandex.com>
2019-12-15 20:49:06 +1000

    add Download list

---
+++ lists/homepage/Download.js
@@ -0,0 +1,24 @@
+const { Text, File, CloudinaryImage, Url } = require('@keystonejs/fields');
+const { fileAdapter, cloudinaryAdapter } = require('../FileAdapters');
+
+module.exports = {
+  fields: {
+    image: {
+      type: CloudinaryImage,
+      adapter: cloudinaryAdapter,
+      isRequired: true
+    },
+    title: {
+      type: Text,
+      isRequired: true
+    },
+    desc: {
+      type: Text,
+      isRequired: true
+    },
+    url: {
+      type: Url,
+      required: true
+    }
+  }
+};
--- lists/homepage/index.js
+++ lists/homepage/index.js
@@ -1,7 +1,9 @@
 const HeroSchema = require('./Hero');
 const AnnouncementSchema = require('./Announcement');
+const DownloadSchema = require('./Download');
 
 module.exports = (keystone) => {
   keystone.createList('Hero', HeroSchema);
   keystone.createList('Announcement', AnnouncementSchema);
+  keystone.createList('Download', DownloadSchema);
 }
