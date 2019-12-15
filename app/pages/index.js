4170c4bbb5d896606656c84e531410277b3bf711
seday72 <seday72@yandex.com>
2019-12-16 00:21:44 +1000

    add API to fetch list data

--- app/pages/index.js
+++ app/pages/index.js
@@ -11,7 +11,6 @@ class App extends Component {
     query GetHeroes {
       allHeroes {
         image {
-          path
           publicUrl
         }
         title
@@ -32,12 +31,82 @@ class App extends Component {
     query GetServices {
       allServices {
         image {
-          path
           publicUrl
         }
         title
         brief
         extended
+      }
+    }`;
+
+    const GET_ALL_JOBS = `
+    query GetJobs {
+      allJobs {
+        title
+        skill
+        code
+        uppercase
+        showmore
+      }
+    }`;
+
+    const GET_ALL_ANNOUNCEMENTS = `
+    query GetAnnouncements {
+      allAnnouncements {
+        image {
+          publicUrl
+        }
+        title
+        brief
+        extended
+      }
+    }`;
+
+    const GET_ALL_DOWNLOADS = `
+    query GetDownloads {
+      allDownloads {
+        image {
+          publicUrl
+        }
+        title
+        desc
+        url
+      }
+    }`;
+
+    const GET_ALL_PORTALS = `
+    query GetPortals {
+      allPortals {
+        image {
+          publicUrl
+        }
+        label
+        desc
+      }
+    }`;
+
+    const GET_ALL_GOODS = `
+    query GetGoods {
+      allGoods {
+        image {
+          publicUrl
+        }
+        label
+        desc
+        currency
+        price
+      }
+    }`;
+
+    const GET_ALL_EVENTS = `
+    query GetEvents {
+      allEvents {
+        image {
+          publicUrl
+        }
+        title
+        desc
+        url
       }
     }`;
 
@@ -52,6 +121,30 @@ class App extends Component {
     axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_SERVICES });
     let services = (axioResult ? axioResult.data.data.allServices : null);
     console.log('All Services:', services);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_JOBS });
+    let jobs = (axioResult ? axioResult.data.data.allJobs : null);
+    console.log('All Jobs:', jobs);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_ANNOUNCEMENTS });
+    let announcements = (axioResult ? axioResult.data.data.allAnnouncements : null);
+    console.log('All Announcements:', announcements);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_DOWNLOADS });
+    let downloads = (axioResult ? axioResult.data.data.allDownloads : null);
+    console.log('All Downloads:', downloads);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_PORTALS });
+    let portals = (axioResult ? axioResult.data.data.allPortals : null);
+    console.log('All Portals:', portals);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_GOODS });
+    let goods = (axioResult ? axioResult.data.data.allGoods : null);
+    console.log('All Goods:', goods);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_EVENTS });
+    let events = (axioResult ? axioResult.data.data.allEvents : null);
+    console.log('All Events:', events);
 
     return { users: axioResult.data };
   }
