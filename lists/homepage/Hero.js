b7be99c5fd25a474fb918621f950ef0fc63dc536
seday72 <seday72@yandex.com>
2019-12-16 05:48:49 +1000

    add a function to check user role and permission

--- lists/homepage/Hero.js
+++ lists/homepage/Hero.js
@@ -1,11 +1,10 @@
-const { Text, File, CloudinaryImage } = require('@keystonejs/fields');
+const { Text, Select, Relationship, File, CloudinaryImage } = require('@keystonejs/fields');
 const { fileAdapter, cloudinaryAdapter } = require('../FileAdapters');
 module.exports = {
   fields: {
     image: {
       type: CloudinaryImage,
       adapter: cloudinaryAdapter,
-      isRequired: true
     },
     title: {
       type: Text,
@@ -15,5 +14,22 @@ module.exports = {
       type: Text,
       isRequired: true
     },
+    author: {
+      type: Relationship,
+      ref: 'User',
+      isRequired: true,
+    },
+    status: {
+      type: Select,
+      options: [{ label: 'Pending', value: 'pending' }, { label: 'Rejected', value: 'rejected' }, { label: 'Approved', value: 'approved' }],
+      defaultValue: 'pending',
+      access: {
+        create: ({ authentication, originalInput }) => { console.log('originalInput', originalInput); return authentication.item.isAdmin || originalInput.status === 'pending'; },
+        update: ({ authentication }) => authentication.item.isAdmin,
+      }
+    },
+  },
+  access: {
+    read: ({ authentication: { item, listKey } }) => ( item.isAdmin || { author: { email: item.email }}),
   }
 }
