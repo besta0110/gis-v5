a29351108173ddde75492154b587bf5804f9cbd1
seday72 <seday72@yandex.com>
2019-12-15 23:57:27 +1000

    add API to fetch here, quicklinks and services

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,7 +1,66 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-);
+import { Component } from 'react';
+import axios from 'axios'
 
-export default Index;
+// var HtmlToReactParser = require('html-to-react').Parser;
+// var htmlToReactParser = new HtmlToReactParser();
+
+class App extends Component {
+  static async getInitialProps({ renderPage }) {
+    let API_ENDPOINT = process.env.API_ENDPOINT;
+    const GET_ALL_HEROES = `
+    query GetHeroes {
+      allHeroes {
+        image {
+          path
+          publicUrl
+        }
+        title
+        subTitle
+      }
+    }`;
+
+    const GET_ALL_QUICKLINKS = `
+    query GetQuickLinks {
+      allQuickLinks {
+        title
+        brief
+        extended
+      }
+    }`;
+
+    const GET_ALL_SERVICES = `
+    query GetServices {
+      allServices {
+        image {
+          path
+          publicUrl
+        }
+        title
+        brief
+        extended
+      }
+    }`;
+
+    let axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_HEROES });
+    let heroes = (axioResult ? axioResult.data.data.allHeroes : null);
+    console.log('All Heroes:', heroes);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_QUICKLINKS });
+    let quickLinks = (axioResult ? axioResult.data.data.allQuickLinks : null);
+    console.log('All QuickLinks:', quickLinks);
+
+    axioResult = await axios.post(API_ENDPOINT, { query: GET_ALL_SERVICES });
+    let services = (axioResult ? axioResult.data.data.allServices : null);
+    console.log('All Services:', services);
+
+    return { users: axioResult.data };
+  }
+
+  render() {
+    return (
+    <div>Hello GIS</div>
+    )
+  };
+}
+
+export default App;
