2ef38d012f82b7a35ded3dc26d05b018ef1e2b40
seday72 <seday72@yandex.com>
2019-12-15 20:56:33 +1000

    add Event list

--- lists/homepage/index.js
+++ lists/homepage/index.js
@@ -1,9 +1,11 @@
 const HeroSchema = require('./Hero');
 const AnnouncementSchema = require('./Announcement');
 const DownloadSchema = require('./Download');
+const EventSchema = require('./Event');
 
 module.exports = (keystone) => {
   keystone.createList('Hero', HeroSchema);
   keystone.createList('Announcement', AnnouncementSchema);
   keystone.createList('Download', DownloadSchema);
+  keystone.createList('Event', EventSchema);
 }
