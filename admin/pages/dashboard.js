c6ed6fc313b11d01e623e5aab7f88cf615875f33
seday72 <seday72@yandex.com>
2019-12-18 15:55:05 +1000

    add notification in dashboard page

--- admin/pages/dashboard.js
+++ admin/pages/dashboard.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import axios from 'axios'
 
 import { Container } from '@arch-ui/layout';
 import { Title } from '@arch-ui/typography';
+import { Alert, AlertGroup } from '@arch-ui/alert';
 
-const Dashboard = () => (
-  <Container>
-    <Title as="h1" margin="both">
-      Dashboard
-    </Title>
-    <p>This is a custom dashboard in the Blog demo project Admin UI.</p>
-    <p>
-      It demonstrates the ability to change the default dashboard and render a custom page instead.
-    </p>
-  </Container>
-);
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { notification: { pendings: { hero: 0 } } };
+    this.API_ENDPOINT = 'http://localhost:3000/admin/api';  // 'http://192.186.142.204:3000/admin/api'
+    this.ADMIN_PATH = 'http://localhost:3000/admin';        // 'http://192.186.142.204:3000/admin'
+  }
+
+  async componentDidMount() {
+    const GET_PENDING_HEROES = `
+    query GetPendingHeros {
+      _allHeroesMeta(where: {status: pending}) {
+        count
+      }
+    }`;
+    
+    let axioResult = await axios.post(this.API_ENDPOINT, { query: GET_PENDING_HEROES });
+    let count = (axioResult ? axioResult.data.data._allHeroesMeta.count : null);
+    console.log('Count of Pending Hero itmes', count);
+    this.setState({ notification: { pendings: { hero: count } } });
+  }
+
+  render() {
+    let heroCount = this.state.notification.pendings.hero;
+    return (
+      <Container>
+        <Title as="h1" margin="both">
+          Dashboard
+        </Title>
+        {heroCount === 0 ? (<p>You have no any notification</p>) : (<Alert appearance="info"><p>You have {heroCount} pending item(s) waiting for your approval in <a href={this.ADMIN_PATH + '/heroes'}>Hero list</a></p></Alert>) }
+      </Container>
+    )
+  }
+}
 
 export default Dashboard;
